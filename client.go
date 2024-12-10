d8fb212f2dabd803a250928be507c520c1d2ec7b
Pa Khan <don@pa-khan.com>
2024-12-11 00:27:56 +0300

    v5

--- client.go
+++ client.go
@@ -4,52 +4,47 @@ import (
 	"context"
 	"fmt"
 	"github.com/budka-tech/configo"
-	"github.com/jackc/pgconn"
-	"github.com/jackc/pgx"
-	"github.com/jackc/pgx/v4/pgxpool"
+	"github.com/jackc/pgx/v5/pgxpool"
 	"log"
 	"time"
 )
 
 type Storage struct {
-	Conn *pgx.Conn
+	Pool *pgxpool.Pool
 }
 
-type Client interface {
-	Exec(ctx context.Context, sql string, arguments ...interface{}) (pgconn.CommandTag, error)
-	Query(ctx context.Context, sql string, args ...interface{}) (pgx.Rows, error)
-	QueryRow(ctx context.Context, sql string, args ...interface{}) pgx.Row
-	Begin(ctx context.Context) (pgx.Tx, error)
-	//BeginTx(ctx context.Context, txOptions pgx.TxOptions)
-	//BeginTxFunc(ctx context.Context, txOptions pgx.TxOptions, f func(pgx.Tx) error) error
-}
-
-func NewClient(ctx context.Context, cfg *configo.Database) (pool *pgxpool.Pool, err error) {
+func NewClient(ctx context.Context, cfg *configo.Database) (*Storage, error) {
 	dsn := Dsn(cfg)
 
-	err = try(func() error {
+	var pool *pgxpool.Pool
+	err := try(func() error {
+		var err error
 		ctx, cancel := context.WithTimeout(ctx, cfg.AttemptDelay)
 		defer cancel()
 
-		pool, err = pgxpool.Connect(ctx, dsn)
+		pool, err = pgxpool.New(ctx, dsn)
 		if err != nil {
-			return fmt.Errorf("Ошибка при подключении к базе данных")
+			return fmt.Errorf("ошибка при создании пула подключений к базе данных: %w", err)
+		}
+
+		err = pool.Ping(ctx)
+		if err != nil {
+			return fmt.Errorf("ошибка при проверке подключения к базе данных: %w", err)
 		}
 
 		return nil
 	}, cfg.MaxAttempts, cfg.AttemptDelay)
 
 	if err != nil {
-		return nil, fmt.Errorf("Не удалось подключиться к базе данных после %v попыток\n", cfg.MaxAttempts)
-	} else {
-		log.Println("Успешное подключение к базе данных!")
+		return nil, fmt.Errorf("не удалось подключиться к базе данных после %v попыток: %w", cfg.MaxAttempts, err)
 	}
 
-	return pool, nil
+	log.Println("Успешное подключение к базе данных!")
+	return &Storage{Pool: pool}, nil
 }
 
 func Dsn(cfg *configo.Database) string {
-	return fmt.Sprintf("%v://%v:%v@%v:%v/%v", cfg.Type, cfg.User, cfg.Password, cfg.Host, cfg.Port, cfg.Name)
+	return fmt.Sprintf("postgres://%s:%s@%s:%d/%s", cfg.User, cfg.Password, cfg.Host, cfg.Port, cfg.Name)
 }
 
 func try(fn func() error, attempts int, delay time.Duration) (err error) {
@@ -57,12 +52,9 @@ func try(fn func() error, attempts int, delay time.Duration) (err error) {
 		if err = fn(); err != nil {
 			time.Sleep(delay)
 			attempts--
-
 			continue
 		}
-
 		return nil
 	}
-
 	return
 }
--- go.mod
+++ go.mod
@@ -5,9 +5,7 @@ go 1.23.2
 require (
 	github.com/budka-tech/configo v0.0.4
 	github.com/golang-migrate/migrate/v4 v4.18.1
-	github.com/jackc/pgconn v1.14.3
-	github.com/jackc/pgx v3.6.2+incompatible
-	github.com/jackc/pgx/v4 v4.18.2
+	github.com/jackc/pgx/v5 v5.7.1
 )
 
 require (
@@ -15,19 +13,14 @@ require (
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/ilyakaznacheev/cleanenv v1.5.0 // indirect
-	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
-	github.com/jackc/fake v0.0.0-20150926172116-812a484cc733 // indirect
-	github.com/jackc/pgio v1.0.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
-	github.com/jackc/pgproto3/v2 v2.3.3 // indirect
-	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
-	github.com/jackc/pgtype v1.14.0 // indirect
-	github.com/jackc/puddle v1.3.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/lib/pq v1.10.9 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	golang.org/x/crypto v0.27.0 // indirect
+	golang.org/x/sync v0.8.0 // indirect
 	golang.org/x/text v0.18.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
